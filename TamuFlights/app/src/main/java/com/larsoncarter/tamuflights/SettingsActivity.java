2d05f04924859d15485df9e9cd0268273004d6b6
Larson Carter <larsoncarter@blazermail.net>
2020-05-13 11:56:59 -0500

    Setup baseline for settings
    I created the settings activity, it is now accessible by the action menu, when clicking settings. Now I need to actually create settings.

---
+++ TamuFlights/app/src/main/java/com/larsoncarter/tamuflights/SettingsActivity.java
@@ -0,0 +1,65 @@
+package com.larsoncarter.tamuflights;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.preference.PreferenceFragmentCompat;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.settings_activity);
+
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.settings, new SettingsFragment())
+                .commit();
+
+        ActionBar actionBar = getSupportActionBar();
+
+        // toolbar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // add back arrow to toolbar
+        if (getSupportActionBar() != null){
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+        }
+
+        if (actionBar != null) {
+
+            actionBar.setDisplayHomeAsUpEnabled(true);
+
+        }
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        // handle arrow click here
+        if (item.getItemId() == android.R.id.home) {
+
+            finish(); // close this activity and return to preview activity (if there is any)
+
+        }
+
+        return super.onOptionsItemSelected(item);
+
+    }
+
+    public static class SettingsFragment extends PreferenceFragmentCompat {
+        @Override
+        public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+            setPreferencesFromResource(R.xml.root_preferences, rootKey);
+        }
+    }
+}
